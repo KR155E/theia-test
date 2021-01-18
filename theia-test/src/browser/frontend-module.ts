0a631a7c57657fedec8115ea0c85ee237ed9e93f
KR155E <c.radke@posteo.de>
2021-01-18 22:46:42 +0100

    Update

--- theia-test/src/browser/frontend-module.ts
+++ theia-test/src/browser/frontend-module.ts
@@ -2,7 +2,7 @@ import { CommandContribution } from "@theia/core";
 import { WebSocketConnectionProvider } from "@theia/core/lib/browser";
 import { ContainerModule } from "inversify";
 import { UsbService, workspacePath } from "../common/usb-service-protocol";
-import { HelloWorldCommandContribution } from "./hello-world-command-contribution";
+import { UsbEventContribution } from "./usb-event-contribution";
 
 export default new ContainerModule((bind) => {
   bind(UsbService).toDynamicValue(ctx => {
@@ -10,5 +10,5 @@ export default new ContainerModule((bind) => {
     return provider.createProxy<UsbService>(workspacePath);
   }).inSingletonScope();
 
-  bind(CommandContribution).to(HelloWorldCommandContribution);
+  bind(CommandContribution).to(UsbEventContribution);
 });
--- theia-test/src/browser/usb-event-contribution.ts
+++ theia-test/src/browser/usb-event-contribution.ts
@@ -2,22 +2,11 @@ import { CommandContribution, CommandRegistry } from "@theia/core";
 import { inject, injectable } from "inversify";
 import { UsbService } from "../common/usb-service-protocol";
 
-export const HelloWorldCommand = {
-    id: 'HelloWorld.command',
-    label: "Hello World"
-};
-
 @injectable()
-export class HelloWorldCommandContribution implements CommandContribution {
+export class UsbEventContribution implements CommandContribution {
     @inject(UsbService) protected readonly usbService: UsbService;
 
     registerCommands(registry: CommandRegistry): void {
-        registry.registerCommand(HelloWorldCommand, {
-            execute: () => {
-                alert('Hello World!')
-            }
-        });
-
         this.usbService.onDeviceConnected((device) => {
             console.log("Frontend: device attached", device);
         });
